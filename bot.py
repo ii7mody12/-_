4cfd1c17f3d5b38de4ddc5bdb480955886a3fae8
MrEnrico33 <44499384+MrEnrico33@users.noreply.github.com>
2018-10-26 21:16:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import requests
 
 client = commands.Bot(command_prefix="!")
 player_dict = dict()
