7bf8edb8357d3f97cab14a29f7202c9a3ffe5668
ii7mody12 <44589893+ii7mody12@users.noreply.github.com>
2019-04-16 19:02:16 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,58 +1,44 @@
 import discord
+from discord import Member
 from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
-import random
+from discord.ext.commands import has_permissions, CheckFailure
 import requests
-import os
 
-client = commands.Bot(command_prefix="!")
-player_dict = dict()
-
-
-@client.event
+Prefix = "-" # BOT PREFIX
+bot = commands.Bot(command_prefix=Prefix)
+antibot = True
+ 
+@bot.event
 async def on_ready():
-    print("Bot ist bereit")
-    
+    print("Im Online Bro {}".format(len(bot.servers)))
+
+@bot.event
+async def on_member_join(member):
+    if antibot is True:
+        if member.bot == True:
+            logs = discord.utils.get(member.server.channels, name="logs")
+            await bot.ban(member)
+            await bot.send_message(logs, f"{member.name} with id {member.id} has banned by bot < antibot > !")
+        if member.bot == False:
+            channel = discord.utils.get(member.server.channels, name="welcome")
+            await bot.send_message(channel, f"{member} Welcome To {member.server.name}")    
 
 
-@client.command(pass_context=True)
-async def play (ctx, url):
-    channel = ctx.message.author.voice_channel
-    await client.join_voice_channel(channel)
-    server = ctx.message.server
-    voice = client.voice_client_in(server)
-    player = await voice.create_ytdl_player(url)
-    player_dict[server.id] = player
-    await client.send_message(ctx.message.channel, "Playing `%s` now" % player.title)
-    player.start()
+@bot.group(pass_context=True)
+async def antibot(ctx):
+    if ctx.invoked_subcommand == None:
+        await bot.send_message(ctx.message.channel, f"{Prefix}antibot on | On Bot Join Will Ban Him !\n{Prefix}antibot off | On Bot Join Will Get Welcome Channel And Send Message To Channel < BOT Welcome To ServerName > ")
+@antibot.command(pass_context=True)
+async def on(ctx):
+    global antibot
+    antibot = True
+    await bot.say("AntiBot Is On !")
+
+@antibot.command(pass_context=True)
+async def off(ctx):
+    global antibot
+    antibot = False
+    await bot.say("AntiBot Is Off !")
 
 
-@client.command(pass_context=True)
-async def stop(ctx):
-    server = ctx.message.server
-    player = player_dict[server.id]
-    player.stop()
-    await client.send_message(ctx.message.channel, "Stopped `%s`" % player.title)
-    del player_dict[server.id]
-
-
-@client.command(pass_context=True)
-async def pause(ctx):
-    server = ctx.message.server
-    player = player_dict[server.id]
-    player.pause()
-    await client.send_message(ctx.message.channel, "Paused `%s`" % player.title)
-
-
-@client.command(pass_context=True)
-async def resume(ctx):
-    server = ctx.message.server
-    player = player_dict[server.id]
-    player.resume()
-    await client.send_message(ctx.message.channel, "Resumed `%s`" % player.title)
-
-
-
-
-client.run(str(os.environ.get('BOT_TOKEN')))
+bot.run(str(os.environ.get('BOT_TOKEN')))
